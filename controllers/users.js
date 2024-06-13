7650b3bd3edd17722ad86994f9d5a297a3e41dfd
ndongaloris <ndongajared@gmail.com>
2024-06-13 21:15:56 +0200

    mongoose working fine

--- controllers/users.js
+++ controllers/users.js
@@ -13,8 +13,7 @@ exports.getAll = async (req, res)=>{
 
 exports.getSingle = async (req, res)=>{
     const userId = new ObjectId(req.params.id);
-    const result = await database.getDatabase().db().collection("users").find({_id: userId});
-    result.toArray().then((users) =>{
+    User.find({_id: userId}).then((users) =>{
         res.setHeader("Content-Type", "application/json");
         res.status(200).json(users[0])
     }).catch((err) => {
@@ -23,51 +22,47 @@ exports.getSingle = async (req, res)=>{
 }
 
 exports.createUser = async(req, res) =>{
-        const newDoc = {
+        const newDoc = new User({
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             middleName: req.body.middleName,
             favoriteColor: req.body.favoriteColor,
             email: req.body.email,
             birthday: req.body.birthday,
-        }
+        })
 
-        await database.getDatabase().db().collection("users").insertOne(newDoc).catch((err) =>{
-            throw Error("an error occur when trying to Post", err);
-        }).then((data) => {
+        newDoc.save(newDoc).then((data) => {
             res.send(data);
+        }).catch((err) =>{
+            throw Error("an error occur when trying to Post", err);
         })
 }
 
 exports.updateUser = async (req, res) =>{
-    try{ 
-        const userId = new ObjectId(req.params.id);
-        const newDoc = {}
-            if (req.body.firstName !== undefined) newDoc.firstName = req.body.firstName;
-            if (req.body.lastName !== undefined) newDoc.lastName= req.body.lastName;
-            if (req.body.middleName !== undefined) newDoc.middleName = req.body.middleName;
-            if (req.body.favoriteColor !== undefined) newDoc.favoriteColor = req.body.favoriteColor;
-            if (req.body.email !== undefined) newDoc.email = req.body.email;
-            if (req.body.birthday !== undefined) newDoc.birthday = req.body.birthday;
-        
-        // const oldDoc = await database.getDatabase().db().collection("users").findOne({});
-        await database.getDatabase().db().collection("users").updateOne({_id: userId}, {$set: newDoc})
-        .then((data) => {
-            res.send(data);
-        });
-    }catch(err){
-        console.log("Update failed", err);
-    }
+    const userId = new ObjectId(req.params.id);
+    const newDoc = {}
+        if (req.body.firstName !== undefined) newDoc.firstName = req.body.firstName;
+        if (req.body.lastName !== undefined) newDoc.lastName= req.body.lastName;
+        if (req.body.middleName !== undefined) newDoc.middleName = req.body.middleName;
+        if (req.body.favoriteColor !== undefined) newDoc.favoriteColor = req.body.favoriteColor;
+        if (req.body.email !== undefined) newDoc.email = req.body.email;
+        if (req.body.birthday !== undefined) newDoc.birthday = req.body.birthday;
+    
+    // const oldDoc = await database.getDatabase().db().collection("users").findOne({});
+    User.updateOne({_id: userId}, {$set: newDoc})
+    .then((data) => {
+        res.send(data);
+    }).catch((err) =>{
+        throw Error("Update failed", err);
+    })
 }
 
 exports.deleteUser = async (req, res) =>{
-    try{
-        const userId = new ObjectId(req.params.id);
-        await database.getDatabase().db().collection("users").deleteOne({_id: userId})
-        .then((data) => {
-            res.send(data);
-        });
-    }catch(err){
-        throw Error("Delete failed", err)
-    }
+    const userId = new ObjectId(req.params.id);
+    await User.deleteOne({_id: userId})
+    .then((data) => {
+        res.send(data);
+    }).catch((err) =>{
+        throw Error("Delete failed", err);
+    })
 }
--- models/users.js
+++ models/users.js
@@ -1,16 +1,17 @@
-const mongoose = require("mongoose");
+module.exports = (mongoose) => {
+    const User = mongoose.model(
+        "users",
+        mongoose.Schema(
+            {
+                firstName: String,
+                lastName: String,
+                middleName: String,
+                favoriteColor: String,
+                email: String,
+                birthday: String,
+            },
+        )
+    );
 
-const {Schema, model} = mongoose;
-
-const userSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    middleName: String,
-    favoriteColor: String,
-    email: String,
-    birthday: String,
-})
-
-const userModel = model("Users", userSchema)
-
-module.exports = userModel;
+    return User;
+};
